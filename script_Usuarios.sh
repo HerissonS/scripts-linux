2d77920109035016ff58772185f28998dffed899
Herisson Silva <antonioherisson@gmail.com>
2022-09-27 22:04:18 -0300

    Atualizacao script

--- script_Usuarios.sh
+++ script_Usuarios.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 sudo su
 
-echo "Criando diretórios..."
+echo "Criação dos diretórios..."
 
 mkdir /publico
 mkdir /adm
 mkdir /ven
 mkdir /sec
 
-echo "Criando grupos..."
+echo "Criação dos grupos..."
 
 groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
-echo "Criando usuários... Senha padrao: Senha123"
+echo "Criação dos usuários... Senha padrão para todos usuários: teste123"
 
-useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt teste123) -G GRP_ADM
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt teste123) -G GRP_ADM
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt teste123) -G GRP_ADM
 
-useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt teste123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt teste123) -G GRP_VEN
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt teste123) -G GRP_VEN
 
-useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
-useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
-useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt teste123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt teste123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt teste123) -G GRP_SEC
 
-echo "Especificando permissão dos diretorios"
+echo "Definindo as permissões"
 
 chown root:GRP_ADM /adm
 chown root:GRP_VEN /ven
@@ -39,4 +39,4 @@ chmod -R 770 /adm
 chmod -R 770 /ven
 chmod -R 770 /sec
 
-echo "Fim"
+echo "Script Finalizado!"
